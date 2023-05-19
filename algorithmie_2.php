b219f08a57b9b00bf8f302c78ae5ea0704d722b7
lurumya <ldwganderson@gmail.com>
2023-05-19 12:11:05 +0200

    before updating packages - pacman

--- algorithmie_2.php
+++ algorithmie_2.php
@@ -2,3 +2,11 @@
 // Ecrire une fonction qui renvoie l’inverse d’un tableau donné en paramètre, 
 // par exemple si on donne [10,50,34,54] il devra renvoyer [57,34,50,10]
 
+function arrayReverseValue($tab) : array {
+    
+
+
+    return $tab;
+}
+
+var_dump(arrayReverseValue([10,50,34,54]));
