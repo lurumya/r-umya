4934a9900f27a4fa46e5a9cdda8c3412a179a4d2
lurumya <ldwganderson@gmail.com>
2023-05-19 14:18:21 +0200

    Partie algorithmie 1 - 3 - 4 terminé

--- algorithmie_3.php
+++ algorithmie_3.php
@@ -3,3 +3,12 @@
 // Ecrire une fonction qui renvoie true 
 // si le nombre envoyé en paramètre est pair, false sinon.
 
+function ftCheckEvenNumber($nbr) : ?bool {
+    if ($nbr % 2 != 0)
+        return false;
+    elseif ($nbr % 2 == 0)
+        return true;
+    return null;
+}
+
+var_dump(ftCheckEvenNumber(45));
