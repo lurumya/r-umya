d12cfae3ba8a5cf2c664f1d2e4eba40be3d57590
Arnaud Merlier <arnaud.merlier@rumya.ch>
2023-05-29 16:43:37 +0200

    Exo 20

--- Fonctions_de_base/exo_20.php
+++ Fonctions_de_base/exo_20.php
@@ -15,30 +15,66 @@ Commentaires :
 =====================================================================================
 */
 
+/**
+ * Retour global sur la structure du code (ca ne veut pas dire que ton code est faux):
+ * Dans une fonction si c'est possible, il est préféréble de n'avoir qu'un seul return, ça rend le code plus facile à lire
+ * Tester le nombre de paramètre est utile en C, en php pas vraiment, si un paramètre est déclaré dans la méthode alors s'il n'est pas présent il y aura une erreur, ton test ne sera jamais appelé
+ *
+ */
+
 function strIsRumya($str = '') : bool {
     Errorhandling::errorArgc();
     if (func_num_args() !== 1) {
         echo 'Wrong number of function arguments !' . "\n";
         exit(255);
     }
+    // ton test pourrait être évité si tu modifies le paramètre comme suit :
+    // function strIsRumya(string $str = '') : bool {
+    // ainsi c'est php qui vérifiera si c'est bien un string
     Errorhandling::errorTypeArgStr($str);
+    // ton test va refuser le paramètre vide '', pourtant c'est autorisé
     Errorhandling::errorEmptyArg($str);
     if (strcmp($str, 'Rumya') === 0)
         return true;
     return false;
 }
 
+
 //strIsRumya('RuMya');
 //strIsRumya('');
+// Erreur php, type mismatch, on attend une chaine de caractère mais c'est un tableau qui est donné
 //strIsRumya(5);
 //strIsRumya();
 //strIsRumya('asdf');
+// Erreur php, impossible d'appeler avec 2 arguments si un seul est attendu
 //strIsRumya('RuMya', 6);
+// Erreur php, impossible d'appeler avec 2 arguments si un seul est attendu
 //strIsRumya('RuMya', 'str');
+// Erreur php, type mismatch, on attend une chaine de caractère mais c'est un tableau qui est donné
 //strIsRumya([]);
 
 //var_dump(strIsRumya('RuMya'));
 //var_dump(strIsRumya(''));
+// Erreur php, impossible d'appeler avec 2 arguments si un seul est attendu
 //var_dump(strIsRumya('RuMya', 'str'));
+// Erreur php, type mismatch, on attend une chaine de caractère mais c'est un int qui est donné
 //var_dump(strIsRumya(59));
 
+
+/**
+ * Ma solution à l'exo :
+ */
+function isRumya(?string $str) : bool {
+    $isRumya = false;
+    if (strcmp($str, 'Rumya') === 0){
+        $isRumya = true;
+    }
+    return $isRumya;
+}
+
+//isRumya('toto') // false
+//isRumya('Rumyo') // false
+//isRumya('Rumya') // true
+//isRumya('toto') // false
+//isRumya() // false
+//isRumya(null) // false
