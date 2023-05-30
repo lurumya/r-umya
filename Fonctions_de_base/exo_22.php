06a17db20dce17e28b46c459ac58e94762ce7f94
lurumya <ldwganderson@gmail.com>
2023-05-30 13:57:40 +0200

    exo 22 - needed help for this way

--- Fonctions_de_base/exo_22.php
+++ Fonctions_de_base/exo_22.php
@@ -19,41 +19,115 @@ function getBiggerArrayValueEasy(?array $array = []) : int {
     Errorhandling::errorArgc();
     return (max($array));
 }
-
 //var_dump(getBiggerArrayValueEasy([45,-65,5,555,2,102,8,3]));
 
-function getBiggerArrayValueHard($array = []) : int {
-    Errorhandling::errorArgc();
-    
-    $size = 0;
-    foreach ($array as $element)
-        $size++;
-
-    $arrayTmp = $array;
-    $sizeTmp = 0;
-    $j = 0;
-    while (($size - 1) > $sizeTmp) {
-        if ($arrayTmp[$sizeTmp] > $arrayTmp[$sizeTmp + 1]) {
-            $j++;
-        }
-        if ($arrayTmp[$sizeTmp] > $arrayTmp[$sizeTmp + 1]) {
-            $tmp = $arrayTmp[$sizeTmp];
-            $tmp1 = $arrayTmp[$sizeTmp + 1];
-            $arrayTmp[$sizeTmp] = $tmp1;
-            $arrayTmp[$sizeTmp + 1] = $tmp;
-        }
-        if ($j > 0) {
-            $sizeTmp = 0;
-            $j = 0;
-        }
-        else
-            $sizeTmp++;
+// J'ai un honte, surtout arpès avoir dit que cet exo était facile.
+// La première version que j'ai faite était effectivement fonctionnelle.
+// Arnaud m'a suggéré de sauvegarder la valeur dans une variable uniquement 
+// si elle est supérieure au maximum que tu as déjà trouvé".
+// J'ai bien galéré un moment et ensuite j'ai demandé de l'aide...
+// Un peu frustrant de pas avoir pensé à ça...
+function getBiggerArrayValueHard(array $array = []): int {
+    $size = count($array);
+    $i = 0;
+    $bigestInt = 0;
+    while ($i < $size) {
+        if ($array[$i] > $bigestInt)
+            $bigestInt = $array[$i];
+        $i++;
     }
-    return $arrayTmp[$size - 1];
+    return $bigestInt;
 }
 
-//var_dump(getBiggerArrayValueHard([45,-65,5,555,2,102,8,3]));
+//echo getBiggerArrayValueHard([43534563,-200,45,-65,5,555,2,102,8343]) . "\n";
 
+
+
+
+
+
+
+/*
+function getBiggerArrayValueHard(array $array = []) : int {
+    //Errorhandling::errorArgc();
+    $size = count($array);
+    $i = 0;
+    $bigestInt = 0;
+    while ($i < $size && ($i + 1 < $size)) {
+
+        if ($array[$i] < $array[$i + 1]) {
+            $bigestInt = $array[$i + 1];
+        }
+        if ($array[$i] > $array[$i + 1]) {
+            $bigestInt = $array[$i];
+        }
+        $i++;
+    }
+    return $bigestInt ;
+}
+getBiggerArrayValueHard([-200,45,-65,5,555,2,102,8,3]);
+*/
+
+
+/*
+function getBiggerArrayValueHard(array $array = []) : int {
+    Errorhandling::errorArgc();
+    //$size = -1;
+    //foreach ($array as $element) {
+    //    $size++;
+    //}
+    $size = count($array);
+
+    $i = 0;
+    $bigestInt = 0;
+    while ($i < $size && ($i + 1 < $size)) {
+
+        if ($array[$i] < $array[$i + 1]) {
+            $bigestInt = $array[$i + 1];
+            $array[$i] = 0;
+            //echo 'AA - $array[$i] = ' . $array[$i] . ' | ' . '$i = ' . $i . ' | ' . '$bigestInt = ' . $bigestInt . "\n";
+            //echo 'AAA - $bigestInt = ' . $bigestInt . '|'. '$tmp = ' . $tmp . "\n";
+        }
+        if ($array[$i] > $array[$i + 1]) {
+            $bigestInt = $array[$i];
+            //echo 'BB - $array[$i] = ' . $array[$i] . ' | ' . '$i = ' . $i . ' | ' . '$bigestInt = ' . $bigestInt . "\n";
+            //echo 'AAA - $bigestInt = ' . $bigestInt . '|'. '$tmp = ' . $tmp . "\n";
+        }
+
+        $i++;
+
+    }
+    //echo 'CCC - $biggerInt = ' . $bigestInt . "\n\n";
+    return $bigestInt ;
+}
+echo getBiggerArrayValueHard([-200,45,-65,5,555,2,102,8,3]) . "\n\n";
+//var_dump(getBiggerArrayValueHard([-200,45,-65,5,555,2,102,8,3]));
+*/
+
+/*
+function getBiggerArrayValueHard(array $array = []) : array {
+    Errorhandling::errorArgc();
+    $size = -1;
+    foreach ($array as $element) {
+        $size++;
+    }
+    $i = 0;
+    $biggerInt = 0;
+    while ($i < $size && ($i + 1 <= $size)) {
+        $tmp = 0;
+        if ($array[$i] < $array[$i + 1]) {
+            $array[$i] = 0;
+            $biggerInt = $array[$i + 1];
+            echo '$array[$i] = ' . $array[$i] . '|' . '$i = ' . $i . "\n";
+        }
+        $i++;
+    }
+    echo '$biggerInt = ' .$biggerInt . "\n";
+    return $array;
+}
+//echo getBiggerArrayValueHard([-200,45,-65,5,555,2,102,8,3]) . "\n\n";
+var_dump(getBiggerArrayValueHard([-200, 45,-65,5,555,2,102,8,3]));
+*/
 
 /**
  * C'est fonctionnel en effet, cependant je pense que ca peut être simplifié
